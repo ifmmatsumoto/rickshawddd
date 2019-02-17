d747034382cc982bfe1ca35896aa76be16dde070
Matsumoto Masayoshi <matsumoto.develop@gmail.com>
2019-02-17 16:57:59 +0900

    first commit

---
+++ src/main/java/com/tutorial/rickshawddd/RickshawdddApplication.java
@@ -0,0 +1,14 @@
+package com.tutorial.rickshawddd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RickshawdddApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RickshawdddApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/tutorial/rickshawddd/RickshawdddApplicationTests.java
@@ -0,0 +1,17 @@
+package com.tutorial.rickshawddd;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RickshawdddApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
